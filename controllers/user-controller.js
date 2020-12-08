ac7a41dc4cafec58816831fa0ca36c2b03ba4572
MBrassey <luc1d_life@protonmail.com>
2020-12-08 00:05:13 -0700

    Add / Remove Friend User-Controller

--- controllers/user-controller.js
+++ controllers/user-controller.js
@@ -8,6 +8,10 @@ const userController = {
         path: "thoughts",
         select: "-__v",
       })
+      .populate({
+        path: "friends",
+        select: "-__v",
+      })
       .select("-__v")
       .then((dbUserData) => res.json(dbUserData))
       .catch((err) => {
@@ -37,12 +41,14 @@ const userController = {
         res.status(400).json(err);
       });
   },
+  
   // create user
   createUser({ body }, res) {
     User.create(body)
       .then((dbUserData) => res.json(dbUserData))
       .catch((err) => res.status(400).json(err));
   },
+
   // update user by id
   updateUser({ params, body }, res) {
     User.findOneAndUpdate({ _id: params.id }, body, { new: true })
@@ -55,6 +61,7 @@ const userController = {
       })
       .catch((err) => res.status(400).json(err));
   },
+
   // delete user
   deleteUser({ params }, res) {
     User.findOneAndDelete({ _id: params.id })
@@ -67,6 +74,45 @@ const userController = {
       })
       .catch((err) => res.status(400).json(err));
   },
+
+    // add friend
+    addFriend({ params }, res) {
+        User.findOneAndUpdate(
+          { _id: params.id },
+          { $push: { friends: params.friendId } },
+          { new: true, runValidators: true }
+        )
+          .populate({
+            path: "friends",
+            select: "-__v",
+          })
+          .select("-__v")
+          .then((dbUserData) => {
+            if (!dbUserData) {
+              res.status(404).json({ message: "No User found with this id!" });
+              return;
+            }
+            res.json(dbUserData);
+          })
+          .catch((err) => res.json(err));
+      },
+
+      // remove friend
+      removeFriend({ params }, res) {
+        User.findOneAndUpdate(
+          { _id: params.id },
+          { $pull: { friends: params.friendId } },
+          { new: true }
+        )
+        .populate({
+            path: "friends",
+            select: "-__v",
+          })
+          .select("-__v")
+          .then((dbUserData) => res.json(dbUserData))
+          .catch((err) => res.json(err));
+      },
+    };
 };
 
 module.exports = userController;
