928d3824fd45256008f1cd956b1c2d49eeff0bc4
MBrassey <luc1d_life@protonmail.com>
2020-12-07 23:05:40 -0700

    + Reaction Model Integration

--- models/Thought.js
+++ models/Thought.js
@@ -1,6 +1,35 @@
 const { Schema, model, Types } = require("mongoose");
 const moment = require("moment");
 
+const ReactionSchema = new Schema(
+  {
+    reactionId: {
+      type: Schema.Types.ObjectId,
+      default: () => new Types.ObjectId(),
+    },
+    reactionBody: {
+      type: String,
+      required: true,
+      maxlength: [280, "Reaction cannot be more than 280 characters"],
+    },
+    username: {
+      type: String,
+      required: true,
+    },
+    createdAt: {
+      type: Date,
+      default: Date.now,
+      get: (createdAtVal) =>
+        moment(createdAtVal).format("MMM DD, YYYY [at] hh:mm a"),
+    },
+  },
+  {
+    toJSON: {
+      getters: true,
+    },
+  }
+);
+
 const ThoughtSchema = new Schema(
   {
     thoughtText: {
@@ -17,7 +46,7 @@ const ThoughtSchema = new Schema(
       type: String,
       required: true,
     },
-    // Add Reactions Later
+    reactions: [ReactionSchema],
   },
   {
     toJSON: {
@@ -27,6 +56,10 @@ const ThoughtSchema = new Schema(
   }
 );
 
-const Thought = model('Thought', ThoughtSchema);
+ThoughtSchema.virtual("reactionCount").get(function () {
+  return this.reactions.length;
+});
+
+const Thought = model("Thought", ThoughtSchema);
 
 module.exports = Thought;
